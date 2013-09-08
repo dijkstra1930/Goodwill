59bb3ad6daa1785c914f27107b976d9f632dcee9
Lyman Cao <lyman@ip-172-31-5-211.us-west-2.compute.internal>
2013-09-08 06:51:46 +0000

    First rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Goodwill::Application.config.secret_key_base = 'b12adb4ff47fd13c34c0e3b94f0138ac86fc94c7bbfa8fe394e80899076cf90212698172b13268a2a93ed6de808787bcc459be77b4913db3beb86617fad89c51'
